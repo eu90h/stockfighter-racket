07b8f4c1d79900179666cc85d6b7b663cc1b102f
Stefan <euler90h@gmail.com>
2015-12-22 23:09:01 -0500

    reworked date/time functions. added fill-time

--- main.rkt
+++ main.rkt
@@ -3,5 +3,6 @@
          (all-from-out "orders.rkt")
          (all-from-out "fills.rkt")
          (all-from-out "quotes.rkt")
-         (all-from-out "feed.rkt"))
-(require "stockfighter.rkt" "orders.rkt" "fills.rkt" "quotes.rkt" "feed.rkt")
+         (all-from-out "feed.rkt")
+         (all-from-out "time.rkt"))
+(require "stockfighter.rkt" "orders.rkt" "fills.rkt" "quotes.rkt" "feed.rkt" "time.rkt")
